33150cf98487943586c698fa88cf25b4da5ab680
AdrianP94 <adrianpena_94@hotmail.com>
2017-11-20 19:27:33 -0600

    Login y registro

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -2,4 +2,10 @@ class EventosController < ApplicationController
     def index
         @eventos = Evento.all
     end
+    def login
+    	@eventos = Evento.all
+    end
+    def register
+    	@eventos = Evento.all
+    end
 end
