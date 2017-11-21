6cf7ba71518acec912ae7e94484c3c6791dac795
jgalvan <galvan.vil@gmail.com>
2017-11-20 19:42:27 -0600

    Merge new views with rails

--- app/controllers/eventos_controller.rb
+++ app/controllers/eventos_controller.rb
@@ -2,10 +2,4 @@ class EventosController < ApplicationController
     def index
         @eventos = Evento.all
     end
-    def login
-    	@eventos = Evento.all
-    end
-    def register
-    	@eventos = Evento.all
-    end
 end
