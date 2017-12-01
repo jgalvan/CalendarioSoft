eb2b55717fc8c0766a4302ad387167d26e6ef788
jgalvan <galvan.vil@gmail.com>
2017-12-01 17:34:46 -0600

    Fix add event showing for non-admin user and validate participants quantity

--- app/models/evento.rb
+++ app/models/evento.rb
@@ -6,6 +6,7 @@ class Evento < ApplicationRecord
   validates :nombre, :lugar, :maxParticipantes, :descripcion, :fechaInicio, :fechaFin, presence: true
   validate :validarFechas 
   validates :maxParticipantes, numericality: { only_integer: true }
+  validate :validarCantidad
 
   def lugares_disponibles
     maxParticipantes - participantes.count
@@ -18,6 +19,12 @@ class Evento < ApplicationRecord
 
     if fechaInicio.present? &&  fechaFin.present? && fechaInicio > fechaFin
       errors.add(:fechaFin, :fin_despues_incio)
+    end
+  end
+
+  def validarCantidad
+    if lugares_disponibles < 0
+      errors.add(:maxParticipantes, "no puede ser menor que la cantidad de participantes registrados (Cantidad actual: #{participantes.count})")
     end
   end
 end
